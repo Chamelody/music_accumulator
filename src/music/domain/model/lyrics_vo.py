8865d3a9955a721756b74a5b6ead403f1e93f596
purplepig4657 <80941516+purplepig4657@users.noreply.github.com>
2023-08-12 13:23:18 +0900

    feat: implement text base value objectsf

--- src/music/domain/model/lyrics_vo.py
+++ src/music/domain/model/lyrics_vo.py
@@ -1,2 +1,12 @@
 class LyricsVO:
-    pass
+    __lyrics: str
+
+    def __init__(self, lyrics: str):
+        self.__lyrics = lyrics
+
+    @property
+    def lyrics(self) -> str:
+        return self.__lyrics
+
+    def __str__(self) -> str:
+        return self.lyrics
--- src/semantic/domain/model/text_vo.py
+++ src/semantic/domain/model/text_vo.py
@@ -1,2 +1,12 @@
 class TextVO:
-    pass
+    __text: str
+
+    def __init__(self, text: str):
+        self.__text = text
+
+    @property
+    def text(self) -> str:
+        return self.__text
+
+    def __str__(self) -> str:
+        return self.text
